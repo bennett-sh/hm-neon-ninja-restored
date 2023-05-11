029f44afa378ad4d8a8f553c1129b9008c63dd62
Bennett <110846042+bennett-sh@users.noreply.github.com>
2023-05-11 19:57:15 +0200

    feat!: initial mod

--- code/src/main.ts
+++ code/src/main.ts
@@ -1,42 +1,47 @@
 
-import { createRepository } from 'repository-script'
 import { createPatch } from 'quickentity-script'
 import { mkdir } from 'fs/promises'
 import path from 'path'
 
-async function savePatch(patch: any, name: string, chunk: number = 0) {
-  const chunkFolder = path.join('..', 'content', `chunk${chunk}`)
+async function savePatch(patch: any, name: string, chunk: number = 0, option: string = 'content') {
+  const chunkFolder = path.join('..', option, `chunk${chunk}`)
 
   await mkdir(chunkFolder, { recursive: true })
   await patch.save(path.join(chunkFolder, `${name}.entity.patch.json`))
 }
 
-async function saveRepo(repo: any, name: string = 'repo', chunk: number = 0) {
-  const chunkFolder = path.join('..', 'content', `chunk${chunk}`)
-
-  await mkdir(chunkFolder, { recursive: true })
-  await repo.save(path.join(chunkFolder, `${name}.repository.json`))
-}
-
 async function createMainPatch() {
-  const patch = createPatch('', '')
+  const patch = createPatch(
+    '[assembly:/_pro/characters/templates/hero/agent47/agent47_outfits.template?/outfit_agent47_reward_neon_heroa_v0.entitytemplate].pc_entitytemplate',
+    '[assembly:/_pro/characters/templates/hero/agent47/agent47_outfits.template?/outfit_agent47_reward_neon_heroa_v0.entitytemplate].pc_entityblueprint',
+  )
+
+  patch
+    .getEntity('59e0361e55d60c9a')
+    .setPropertyValue('ConstantVector1D_07_Value', 90)
 
   await savePatch(patch, 'main')
 }
 
-async function createRepositoryPatch() {
-  const repo = createRepository()
+async function createUnplayablePatch() {
+  const patch = createPatch(
+    '[assembly:/_pro/characters/templates/hero/agent47/agent47_outfits.template?/outfit_agent47_reward_neon_heroa_v0.entitytemplate].pc_entitytemplate',
+    '[assembly:/_pro/characters/templates/hero/agent47/agent47_outfits.template?/outfit_agent47_reward_neon_heroa_v0.entitytemplate].pc_entityblueprint',
+  )
 
-  await saveRepo(repo)
+  patch
+    .getEntity('59e0361e55d60c9a')
+    .setPropertyValue('ConstantVector1D_07_Value', 1000)
+
+  await savePatch(patch, 'main', 0, 'unplayable')
 }
 
 async function main() {
   console.log('Making main patch...')
   await createMainPatch()
   console.log('Done.')
-
-  console.log('Making main repository patch...')
-  await createRepositoryPatch()
+  console.log('Making unplayable patch...')
+  await createUnplayablePatch()
   console.log('Done.')
 }
 
